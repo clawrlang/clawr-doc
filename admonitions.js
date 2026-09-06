4ea877c7045c7c959abbc78f4d2d1faf572b21fa
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-09-06 09:05:27 +0200

    Make nicer formatting

--- admonitions.js
+++ admonitions.js
@@ -5,15 +5,17 @@ function formatAdmonitions() {
     for (const p of calloutPs) {
         const bq = p.parentNode
         const type = p.textContent.match(/^\[!(.*)\]/)[1]
-        if (/^\[!(.*)\]$/.test(p.textContent)) p.remove()
-        else p.textContent = p.textContent.replace(/^\[!(.*)\]/, '')
+
+        if (/^\[!(.*)\]$/.test(p.textContent))
+            p.textContent = p.textContent.replace(/^\[!(.*)\]/, $1)
+        else p.textContent = p.textContent.replace(/^\[!(.*)\]\s*/, '')
+
         bq.classList.add('admonition', type)
 
         const icon = document.createElement('i')
         icon.setAttribute('data-lucide', ICONS[type.toUpperCase()])
         icon.classList.add('admonition-icon', type)
-        bq.appendChild(icon)
-        icon.moveBefore(bq.firstChild)
+        p.insertBefore(icon, p.firstChild)
     }
 }
 
